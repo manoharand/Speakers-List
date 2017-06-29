27e018fd06c14b1af1204c20fc4ad83e9dc98bdb
Divya Manoharan <dkm45@case.edu>
2017-06-29 19:39:12 -0400

    First commit

---
+++ Assembly.java
@@ -0,0 +1,44 @@
+public class Assembly {
+
+  private String name;
+  private int talked;
+  private String demographic;
+
+  public Assembly(String name) {
+    this.name = name;
+  }
+
+  public Assembly(String name, int talked) {
+    this.name = name;
+    this.talked = talked;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void incrementTalked() {
+    this.talked++;
+  }
+
+  public int getTalked() {
+    return this.talked;
+  }
+
+	public void setTalked(int talked) {
+		this.talked = talked;
+	}
+
+  public void setDemographic(String demographic) {
+    this.demographic = demographic;
+  }
+
+  public String getDemographic() {
+    return this.demographic;
+  }
+
+}
